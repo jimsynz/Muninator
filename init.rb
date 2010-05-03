6e6df59d96203e6b7749017c350177266fd48442
James Harton <jamesotron@gmail.com>
2010-05-03 12:56:28 +1200

    Added acts_as_munin_plugin

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
 require File.join(File.dirname(__FILE__), 'lib', 'muninator.rb')
+require File.join(File.dirname(__FILE__), 'lib', 'acts_as_munin_plugin.rb')
+ActiveRecord::Base.send(:include, MashdCc::Acts::MuninPlugin)
---
+++ lib/acts_as_munin_plugin.rb
@@ -0,0 +1,41 @@
+module MashdCc
+  module Acts
+    module MuninPlugin
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def acts_as_munin_plugin(opts={})
+          model = self
+          defaults = { :category => 'models', :label => "#{model.name} Usage" }
+          opts = defaults.merge(opts)
+          self.class_eval <<-RUBY
+            module ::Muninator
+              module Commands
+                class #{model.name}Usage
+                  def self.config
+                    "graph_title #{opts[:label]}\n" +
+                    "graph_category #{opts[:category]}\n" +
+                    "graph_info Model count for model #{model.name}\n" +
+                    "graph_vlabel models\n" +
+                    "all.label All models\n" +
+                    "updated.label Updated\n" +
+                    "created.label Created\n"
+                  end
+                  def self.fetch
+                    "all.value " + #{model.name}.all.count.to_s + "\n" +
+                    "updated.value " + #{model.name}.find(:all, :conditions => [ "updated_at >= ?", 5.minutes.ago ]).count.to_s + "\n" +
+                    "created.value " + #{model.name}.find(:all, :conditions => [ "created_at >= ?", 5.minutes.ago ]).count.to_s + "\n"
+                  end
+                end
+              end
+            end
+          RUBY
+        end
+      end
+
+    end
+  end
+end
