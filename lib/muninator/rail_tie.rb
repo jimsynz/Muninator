d0ed5b23294a1d17e7d880dc1ef49060477fdc5b
Niklas Hofer <niklas+dev@lanpartei.de>
2011-03-11 00:49:31 +0100

    add app/munin to Muninator::Template.paths

--- lib/muninator/rail_tie.rb
+++ lib/muninator/rail_tie.rb
@@ -5,6 +5,7 @@ module Muninator
       ActionController::Base.send(:include, MashdCc::Controller::Munin)
       if Rails.env.production? || Rails.env.development?
         Muninator::Commands.search_paths << Rails.root.join('app/munin').to_s
+        Muninator::Template.paths << Rails.root.join('app/munin').to_s
         Muninator.from_config
       end
     end
